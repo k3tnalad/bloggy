b2418f68ea4356d988e8218d3dda2e280419588f
k3tnalad <mr.lawyered@gmail.com>
2022-10-01 13:57:57 +0300

    styles on various pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,mjs,astro}"],
   theme: {
+    colors: {
+      'light-100': '#F2EFE9',
+      'dark-100': '#252627',
+      'accent': '#FA9500',
+      'font-light': '#1E1F20',
+      'font-dark': '#fff',
+    },
     extend: {
       fontFamily: {
         'dosis': ['Dosis', 'sans-serif'],
